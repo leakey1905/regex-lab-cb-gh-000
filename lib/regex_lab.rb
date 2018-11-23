ef4dff2d6e28b6c5fef0ec0340518ac04dfd3817
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 17:56:05 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^\d{10}$/)
+  if !!phone.match(/^\d{10}$/)
+    return true
+
+  end
 end
